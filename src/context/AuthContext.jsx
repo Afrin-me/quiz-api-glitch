9ad5ea1be12e35bdcb50128a55b55b505343c3e8
Afrin-me <shaikhafrin1125@gmail.com>
2025-02-01 18:01:47 +0530

    third commit

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 
 export const AuthContext = createContext()
 const AuthContextProvider=({children})=> {
  
+const[authenticated, setAuthenticated] = useState()
+const[token,setToken] = useState(null)
 
+const navigate= useNavigate()
+
+useEffect(()=>{
+    console.log(token)
+},[token])
   return (
     <div>
         <AuthContextProvider>
